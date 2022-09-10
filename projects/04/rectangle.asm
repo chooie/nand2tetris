7f4eff501edd328d29efa1ef23735841171fbcc8
Charlie Hebert <charlie.hebert92@gmail.com>
2022-09-10 16:55:06 +0100

    Quick test program rectangle.asm

---
+++ projects/04/rectangle.asm
@@ -0,0 +1,44 @@
+// Program: rectangle.asm
+// Draw's a filled rectangle at the screen's top-left corner, with width of 16
+// pixels and height of RAM[0] pixels.
+// Usage: put a non-negative number (rectangle's height) in RAM[0].
+
+  @SCREEN
+  D=A
+  @addr
+  M=D // addr = 16384
+      // (screen's base address)
+  
+  @0
+  D=M
+  @n
+  M=D // n = RAM[0]
+
+  @i
+  M=0 // i = 0
+
+(LOOP)
+  @i
+  D=M
+  @n
+  D=D-M
+  @END
+  D;JGT // if (i > n) goto END
+
+  // otherwise
+  @addr
+  A=M
+  M=-1 // RAM[addr]=1111111111111111
+
+  @i
+  M=M+1 // i = i + 1
+  @32
+  D=A
+  @addr
+  M=D+M // addr = addr + 32 (remember 16 bits for each address)
+  @LOOP
+  0;JMP // goto LOOP
+
+(END)
+  @END
+  0;JMP
