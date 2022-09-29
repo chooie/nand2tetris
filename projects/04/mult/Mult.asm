d79e1a8d66718cbd3de40b0bc1a7559b10bcb752
Charlie Hebert <charlie.hebert92@gmail.com>
2022-09-29 02:34:59 +0100

    Finish Mult.asm

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,78 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// PSEUDOCODE
+//
+// int firstNumber = (load R0);
+// int secondNumber = (load R1);
+// int sum = 0;
+
+// for (int i = 0; i < firstNumber; i += 1) {
+//   sum = sum + secondNumber;
+// }
+// 
+// (load into R2 sum)
+//
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Load R0
+@0
+D=M
+// firstNumber = (load R0)
+@firstNumber
+M=D
+
+// Load R1
+@1
+D=M
+
+// Reset R2 to 0
+@2
+M=0
+
+// secondNumber = (load R1)
+@secondNumber
+M=D
+
+// sum = 0
+@sum
+M=0
+
+// i = 0
+@i
+M=0
+
+(LOOP)
+  @i
+  D=M
+  
+  @firstNumber
+  D=D-M
+
+  @FINISHMULT
+  D;JGE // if (i >= firstNumber) goto FINISHMULT
+
+  // otherwise
+
+  @secondNumber
+  D=M
+  @sum
+  M=D+M // sum = secondNumber + sum
+
+  @i
+  M=M+1 // i = i +1
+
+  @LOOP
+  0;JMP // goto LOOP
+
+(FINISHMULT)
+@sum
+D=M
+
+@2
+M=D // RAM[2] = sum
+
+(END)
+  @END
+  0;JMP
