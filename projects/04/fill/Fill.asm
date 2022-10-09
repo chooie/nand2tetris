c758b3aab008c201d51ee11bd83c662a46ce3138
Charlie Hebert <charlie.hebert92@gmail.com>
2022-10-09 23:01:47 +0100

    Add partial implementation of fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,94 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// while true:
+//   get the key
+//
+//   if the key is not 0:
+//     set paint to black (-1)
+//   else (if the key is 0):
+//     set paint to white (0)
+//
+//   loop through every pixel
+//     set it to paint value
+
+// @SCREEN // get screen base address (16384)
+        // 256 rows of 512 pixels per row
+
+// @KBD // get keyboard address (24576)
+
+@32
+D=A
+@numbersPerRow
+M=D
+
+@256
+D=A
+@allRows
+M=D
+
+@paint
+M=0 // By default, paint white
+
+(LOOP)
+  @currentRow
+  M=0
+
+  @currentColumn
+  M=0
+
+  @SCREEN
+  D=A
+  @currentAddress
+  M=D
+
+  @KBD
+  D=M // Read keyboard input
+
+  @NOKEYPRESSED
+  D;JEQ // Jump if keyboard value == 0
+
+  (KEYISPRESSED)
+  // Set to black
+  @paint
+  M=-1
+
+  @ENDKEYBOARDHANDLING
+  0;JMP
+
+  (NOKEYPRESSED)
+  // Set to white
+  @paint
+  M=0
+
+  (ENDKEYBOARDHANDLING)
+
+  (ROWSLOOP)
+    @currentRow
+    D=M
+    @allRows
+    D=D-M
+    @ENDROWSLOOP
+    D;JGE // Stop painting if we've covered all rows
+
+    @paint
+    D=M // Get paint value
+    @currentAddress
+    A=M
+    M=D // Paint to pixels with paint value
+
+    // increment current row
+    @currentRow
+    M=M+1
+
+    @32
+    D=A
+    @currentAddress
+    M=D+M // Increment address by 32 (512 pixels) to get to next row
+
+    @ROWSLOOP
+    0;JMP
+  (ENDROWSLOOP)
+
+  @LOOP
+  0;JMP // Infinitely loop
