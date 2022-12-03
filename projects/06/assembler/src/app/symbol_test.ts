14d3e8dd2cb21ca1af9d1417095d1f650ac33574
Charlie Hebert <charlie.hebert92@gmail.com>
2022-12-03 17:42:01 +0000

    Finish symbol table

--- projects/06/assembler/src/app/symbol_test.ts
+++ projects/06/assembler/src/app/symbol_test.ts
@@ -1,10 +1,44 @@
-import { assertEquals } from "@utils/asserts.ts";
+import { assertThrows, assertEquals } from "@utils/asserts.ts";
 import { describe, it } from "@utils/bdd.ts";
 
 import * as symbol from "./symbol.ts";
 
 describe("Symbol", () => {
-  it("is true", () => {
-    assertEquals(symbol.makeSymbolTable(), {});
+  it("makes a symbol table", () => {
+    assertEquals(
+      symbol.makeSymbolTable().symbolTable,
+      symbol.PREDEFINED_SYMBOL_TABLE,
+    );
+  });
+
+  it("can check if symbol table contains a symbol", () => {
+    const symbolTable = symbol.makeSymbolTable();
+
+    assertEquals(symbolTable.doesContain("LOOP"), true);
+    assertEquals(symbolTable.doesContain("FOOBAR"), false);
+  });
+
+  it("can add a new symbol", () => {
+    const symbolTable = symbol.makeSymbolTable();
+    symbolTable.add("FOOBAR", null);
+
+    assertEquals(symbolTable.doesContain("FOOBAR"), true);
+    assertThrows<Error>(
+      () => {
+        symbolTable.add("LOOP", 123);
+      },
+      Error,
+      "Symbol already exists",
+    );
+  });
+
+  it("can get the address of a symbol", () => {
+    const symbolTable = symbol.makeSymbolTable();
+    symbolTable.add("FOOBAR", null);
+    symbolTable.add("SOME_SYMBOL", 123);
+
+    assertEquals(symbolTable.getSymbolAddress("LOOP"), 4);
+    assertEquals(symbolTable.getSymbolAddress("FOOBAR"), null);
+    assertEquals(symbolTable.getSymbolAddress("SOME_SYMBOL"), 123);
   });
 });
