15bda11931bb0424895a56cb58ad9c7dd666d081
Charlie Hebert <charlie.hebert92@gmail.com>
2022-12-04 16:34:27 +0000

    Can make a binary for assembling asm hack code

--- projects/06/assembler/src/app/run.ts
+++ projects/06/assembler/src/app/run.ts
@@ -1,15 +1,31 @@
 import * as assembler from "./assembler.ts";
 
-const filePath = Deno.args[0];
+await run();
 
-const sourceCode = await assembler.readTextFile(filePath);
+async function run() {
+  const filePath = Deno.args[0];
 
-if (assembler.isReadTextFileError(sourceCode)) {
-  throw new Error(JSON.stringify(sourceCode, undefined, 2));
+  if (!filePath) {
+    console.log("You must pass a filePath");
+    return;
+  }
+
+  if (filePath === "") {
+    console.log("filePath must not be empty");
+    return;
+  }
+
+  const sourceCode = await assembler.readTextFile(filePath);
+
+  if (assembler.isReadTextFileError(sourceCode)) {
+    throw new Error(JSON.stringify(sourceCode, undefined, 2));
+  }
+
+  const machineCode = assembler.assemble(sourceCode);
+
+  const destinationFilePath = assembler.getDestinationFilePath(filePath);
+
+  await assembler.writeTextFile(destinationFilePath, machineCode);
+
+  console.log(`Generated ${destinationFilePath}`);
 }
-
-const machineCode = assembler.assemble(sourceCode);
-
-const destinationFilePath = assembler.getDestinationFilePath(filePath);
-
-await assembler.writeTextFile(destinationFilePath, machineCode);
